1d7c5a1c3b134f78cc84e0c33b06471102e99a1e
OkashaTanoli <okashatanoli12345@gmail.com>
2022-11-19 01:34:01 +0500

    first commit

---
+++ commonjs/b.js
@@ -0,0 +1,14 @@
+// function add(a,b) {
+//     return a+b
+// }
+// console.log(add(10,20))
+
+
+// const obj = require("./a")
+
+// console.log(obj.sub(30, 20))
+// console.log(obj.add(30, 20))
+
+// console.log(add(30,40))
+// console.log(add(50,70))
+
---
+++ index.js
@@ -0,0 +1,72 @@
+// let a = 10
+// a = "10"
+// console.log(typeof a)
+
+
+// let a = 20
+// let c= 10
+// let b = a++ - c--
+// console.log(a)
+// console.log(b)
+
+// a++
+// ++a
+// a--
+// --a
+
+
+
+// let a = 20
+// // a = a + 2;
+// a+=2
+// console.log(a)
+
+
+
+
+
+// let a = 10
+// let b = 10
+// console.log(a <= b)
+
+// let a = 'uskdrfyrfhoe'
+
+
+// if(a === 10){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// console.log(a === 10 ? "true" : a > 10 ? "uskdrfyrfhoe" : "false")
+
+
+
+
+// console.log(true && true && false)
+
+//(false        false         true)     false
+
+
+
+// if (null) {
+//     console.log("if wala chal gaya")
+// }
+// else {
+//     console.log("else wala chal gaya")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
